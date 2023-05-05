10683d520a58d4f27367533aa853ea633e8d5224
Thomas Yu <thisisthomasyu@gmail.com>
2023-05-05 21:28:43 +0800

    Added style and default value for data row

--- CRUD_App/src/components/AddButton.jsx
+++ CRUD_App/src/components/AddButton.jsx
@@ -4,49 +4,123 @@ const AddButton = ({ items, setItems }) => {
   const [showInput, setShowInput] = useState(true);
   const [date, setDate] = useState('');
   const [name, setName] = useState('');
-  const [objectList, setObjectList] = useState({ a: '', b: '', c: '' });
-  const [quantity, setQuantity] = useState('');
+  const [objectList, setObjectList] = useState({ a: '', b: '', c: '' , d: '', e: '', f: '', g: '', h: '', i: '', j: '', k: '', l: '', m: '', n: ''});
+  const [lorry, setLorry] = useState('');
 
   const addItem = () => {
-    const newItem = { id: Date.now(), date, name, objectList, quantity };
-    setItems([...items, newItem]);
-    setShowInput(true);
+    if (date && name && lorry) {
+      const newItem = {
+        id: Date.now(), date, name, lorry, objectList: setDefaultValues(objectList),
+      };
+      setItems([...items, newItem]);
+      setShowInput(true);
+    } else {
+      alert("You have not complete the input yet");
+    }
   };
+  
 
   const handleObjectChange = (e, key) => {
     setObjectList({ ...objectList, [key]: e.target.value });
   };
 
+  const resetInputFields = () => {
+    setDate('');
+    setName('');
+    setLorry('');
+    setObjectList({
+      a: '', b: '', c: '', d: '', e: '', f: '', g: '',
+      h: '', i: '', j: '', k: '', l: '', m: '', n: '',
+    });
+  };
+
+  const setDefaultValues = (inputObject) => {
+    const outputObject = {};
+    for (const key in inputObject) {
+      outputObject[key] = inputObject[key] === '' ? '0' : inputObject[key];
+    }
+    return outputObject;
+  };
+  
   if (showInput == false) {
     return (
       <div>
         <div className="form-row">
           <div className="form-group">
             <label>Date:</label>
-            <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+            <input type="date" value={date} onChange={(e) => setDate(e.target.value)}/>
           </div>
           <div className="form-group">
             <label>Name:</label>
-            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
           </div>
           <div className="form-group">
-            <label>Object A:</label>
-            <input type="text" value={objectList.a} onChange={(e) => handleObjectChange(e, 'a')} />
+            <label>Lorry:</label>
+            <input type="text" value={lorry} onChange={(e) => setLorry(e.target.value)}/>
           </div>
           <div className="form-group">
-            <label>Object B:</label>
+            <label>C12:</label>
+            <input type="text" value={objectList.a} onChange={(e) => handleObjectChange(e, 'a')}/>
+          </div>
+          <div className="form-group">
+            <label>C12 Tong:</label>
             <input type="text" value={objectList.b} onChange={(e) => handleObjectChange(e, 'b')} />
           </div>
           <div className="form-group">
-            <label>Object C:</label>
+            <label>C14:</label>
             <input type="text" value={objectList.c} onChange={(e) => handleObjectChange(e, 'c')} />
           </div>
           <div className="form-group">
-            <label>Quantity:</label>
-            <input type="number" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
+            <label>C14 Tong:</label>
+            <input type="text" value={objectList.d} onChange={(e) => handleObjectChange(e, 'd')} />
+          </div>
+          <div className="form-group">
+            <label>14C:</label>
+            <input type="text" value={objectList.e} onChange={(e) => handleObjectChange(e, 'e')} />
+          </div>
+          <div className="form-group">
+            <label>14C Tong:</label>
+            <input type="text" value={objectList.f} onChange={(e) => handleObjectChange(e, 'f')} />
+          </div>
+          <div className="form-group">
+            <label>C50:</label>
+            <input type="text" value={objectList.g} onChange={(e) => handleObjectChange(e, 'g')} />
+          </div>
+          <div className="form-group">
+            <label>C50 Tong:</label>
+            <input type="text" value={objectList.h} onChange={(e) => handleObjectChange(e, 'h')} />
+          </div>
+          <div className="form-group">
+            <label>Gas Payment:</label>
+            <input type="text" value={objectList.i} onChange={(e) => handleObjectChange(e, 'i')} />
+          </div>
+          <div className="form-group">
+            <label>Hutang:</label>
+            <input type="text" value={objectList.j} onChange={(e) => handleObjectChange(e, 'j')} />
+          </div>
+          <div className="form-group">
+            <label>Tong Payment:</label>
+            <input type="text" value={objectList.k} onChange={(e) => handleObjectChange(e, 'k')} />
+          </div>
+          <div className="form-group">
+            <label>Bayar Hutang:</label>
+            <input type="text" value={objectList.l} onChange={(e) => handleObjectChange(e, 'l')} />
+          </div>
+          <div className="form-group">
+            <label>Pinjam Tong:</label>
+            <input type="text" value={objectList.m} onChange={(e) => handleObjectChange(e, 'm')} />
+          </div>
+          <div className="form-group">
+            <label>Pulang Tong:</label>
+            <input type="text" value={objectList.n} onChange={(e) => handleObjectChange(e, 'n')} />
           </div>
         </div>
-        <button onClick={addItem}>Done</button>
+        
+        <button onClick={() =>{
+          addItem();
+          resetInputFields();
+          }
+        }>Done</button>
         <button onClick={() => setShowInput(true)}>Cancel</button>
       </div>
     );
--- CRUD_App/src/components/Item.jsx
+++ CRUD_App/src/components/Item.jsx
@@ -10,9 +10,10 @@ const Item = ({ item, updateItem, deleteItem }) => {
   };
 
   const handleObjectChange = (e, key) => {
+    // objectList = setDefaultValues(objectList);
     setEditedItem({
       ...editedItem,
-      objectList: { ...editedItem.objectList, [key]: e.target.value },
+      objectList: { ...editedItem.objectList, [key]: e.target.value }
     });
   };
 
@@ -21,33 +22,85 @@ const Item = ({ item, updateItem, deleteItem }) => {
     setIsEditing(false);
   };
 
+  const setDefaultValues = (inputObject) => {
+    const outputObject = {};
+    for (const key in inputObject) {
+      outputObject[key] = inputObject[key] === '' ? '0' : inputObject[key];
+    }
+    return outputObject;
+  };
+
   if (isEditing) {
     return (
-      <div className="item">
+      <div className="edit_item item">
         <div className="form-row">
           <div className="form-group">
             <label>Date:</label>
-            <input type="date" value={editedItem.date} onChange={handleChange} name="date" />
+            <input type="date" value={editedItem.date} onChange={handleChange} name="date" required/>
           </div>
           <div className="form-group">
             <label>Name:</label>
-            <input type="text" value={editedItem.name} onChange={handleChange} name="name" />
+            <input type="text" value={editedItem.name} onChange={handleChange} name="name" required/>
           </div>
           <div className="form-group">
-            <label>Object A:</label>
+            <label>Lorry:</label>
+            <input type="text" value={editedItem.lorry} onChange={handleChange} name="lorry" required/>
+          </div>
+          <div className="form-group">
+            <label>C12:</label>
             <input type="number" value={editedItem.objectList.a} onChange={(e) => handleObjectChange(e, 'a')} />
           </div>
           <div className="form-group">
-            <label>Object B:</label>
+            <label>C12 Tong:</label>
             <input type="number" value={editedItem.objectList.b} onChange={(e) => handleObjectChange(e, 'b')} />
           </div>
           <div className="form-group">
-            <label>Object C:</label>
+            <label>C14:</label>
             <input type="number" value={editedItem.objectList.c} onChange={(e) => handleObjectChange(e, 'c')} />
           </div>
           <div className="form-group">
-            <label>Quantity:</label>
-            <input type="number" value={editedItem.quantity} onChange={handleChange} name="quantity" />
+            <label>C14 Tong:</label>
+            <input type="number" value={editedItem.objectList.d} onChange={(e) => handleObjectChange(e, 'd')} />
+          </div>
+          <div className="form-group">
+            <label>14C:</label>
+            <input type="number" value={editedItem.objectList.e} onChange={(e) => handleObjectChange(e, 'e')} />
+          </div>
+          <div className="form-group">
+            <label>14C Tong:</label>
+            <input type="number" value={editedItem.objectList.f} onChange={(e) => handleObjectChange(e, 'f')} />
+          </div>
+          <div className="form-group">
+            <label>C50:</label>
+            <input type="number" value={editedItem.objectList.g} onChange={(e) => handleObjectChange(e, 'g')} />
+          </div>
+          <div className="form-group">
+            <label>C50 Tong:</label>
+            <input type="number" value={editedItem.objectList.h} onChange={(e) => handleObjectChange(e, 'h')} />
+          </div>
+          <div className="form-group">
+            <label>GasPayment:</label>
+            <input type="number" value={editedItem.objectList.i} onChange={(e) => handleObjectChange(e, 'i')} />
+          </div>
+          <div className="form-group">
+            <label>Hutang:</label>
+            <input type="number" value={editedItem.objectList.j} onChange={(e) => handleObjectChange(e, 'j')} />
+          </div>
+          <div className="form-group">
+            <label>Tong Payment:</label>
+            <input type="number" value={editedItem.objectList.k} onChange={(e) => handleObjectChange(e, 'k')} />
+          </div>
+          <div className="form-group">
+            <label>Bayar Hutang:</label>
+            <input type="number" value={editedItem.objectList.l} onChange={(e) => handleObjectChange(e, 'l')} />
+          </div>
+          <div className="form-group">
+            <label>Pinjam tong:</label>
+            <input type="number" value={editedItem.objectList.m} onChange={(e) => handleObjectChange(e, 'm')} />
+          </div>
+          <div className="form-group">
+            <label>Pulang tong:</label>
+            <input type="number" value={editedItem.objectList.n} onChange={(e) => handleObjectChange(e, 'n')} />
           </div>
         </div>
         <div className="item-buttons">
@@ -59,19 +112,31 @@ const Item = ({ item, updateItem, deleteItem }) => {
   }
 
   return (
-    <div>
+    <div >
       {/* <h id="listTitle">Recent Add:</h> */}
-      <div className="item">
-        <div>Date: {item.date}</div>
-        <div>Name: {item.name}</div>
-        <div>Object A: {item.objectList.a}</div>
-        <div>Object B: {item.objectList.b}</div>
-        <div>Object C: {item.objectList.c}</div>
-        <div>Quantity: {item.quantity}</div>
-        <div className="item-buttons">
+      <div className="show_item item">
+        <div><span>Date: </span><br></br>{item.date}</div>
+        <div><span>Name: </span><br></br>{item.name}</div>
+        <div><span>Lorry: </span><br></br>{item.lorry}</div>
+        <div><span>C12: </span><br></br>{item.objectList.a}</div>
+        <div><span>C12 Tong: </span><br></br>{item.objectList.b}</div>
+        <div><span>C14: </span><br></br>{item.objectList.c}</div>
+        <div><span>C14 Tong: </span><br></br> {item.objectList.d}</div>
+        <div><span>14C: </span><br></br>{item.objectList.e}</div>
+        <div><span>14C Tong: </span><br></br>{item.objectList.f}</div>
+        <div><span>C50: </span><br></br>{item.objectList.g}</div>
+        <div><span>C50 Tong: </span><br></br>{item.objectList.h}</div>
+        <div><span>Gas Payment: </span><br></br>{item.objectList.i}</div>
+        <div><span>Hutang: </span><br></br>{item.objectList.j}</div>
+        <div><span>Tong Payment: </span><br></br>{item.objectList.k}</div>
+        <div><span>Bayar Hutang: </span><br></br>{item.objectList.l}</div>
+        <div><span>Pinjam tong: </span><br></br>{item.objectList.m}</div>
+        <div><span>Pulang tong: </span><br></br>{item.objectList.n}</div>
+        
+      </div>
+      <div className="item-buttons">
           <button onClick={() => setIsEditing(true)}>Edit</button>
           <button onClick={() => deleteItem(item.id)}>Delete</button>
-        </div>
       </div>
     </div>
   );
