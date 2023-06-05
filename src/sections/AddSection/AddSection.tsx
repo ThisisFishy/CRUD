51ebb84975a16b305a61663f626a0603720187d1
Thomas Yu <thisisthomasyu@gmail.com>
2023-06-05 23:07:29 +0800

    try commit in 'Next' branch

--- src/sections/AddSection/AddSection.tsx
+++ src/sections/AddSection/AddSection.tsx
@@ -15,7 +15,7 @@ export const AddSection = (props: AddSectionProps) => {
 
     return (
         <section>
-            <SectionHeader title="Add Section" />
+            <SectionHeader title="Add Sectionlaalaa" />
 
             {isActive ?
                 <ActiveSubsection
