73dbf3b9986487ed3ae306345a2c0c1868db0fdd
Thomas Yu <thisisthomasyu@gmail.com>
2023-06-05 23:01:03 +0800

    Revert "test for the new branch"
    This reverts commit 899a063569f5b2107ecf090c74175a5dcd9a6986.

--- src/sections/AddSection/AddSection.tsx
+++ src/sections/AddSection/AddSection.tsx
@@ -15,7 +15,7 @@ export const AddSection = (props: AddSectionProps) => {
 
     return (
         <section>
-            <SectionHeader title="Add Section lalala" />
+            <SectionHeader title="Add Section" />
 
             {isActive ?
                 <ActiveSubsection
