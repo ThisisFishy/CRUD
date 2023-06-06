fd76a3a874a75f43699afe8d0409e2840b185881
Thomas Yu <thisisthomasyu@gmail.com>
2023-06-06 18:37:02 +0800

    disabling PurchaseAddedSection and SalesAddedSection

--- pages/sections/PurchaseAddedSection.tsx
+++ pages/sections/PurchaseAddedSection.tsx
@@ -1,24 +1,24 @@
-import { GridRowsProp } from "@mui/x-data-grid"
+// import { GridRowsProp } from "@mui/x-data-grid"
 import PurchaseDataTable from "../../components/PurchaseDataTable";
 import SectionHeader from "../../components/SectionHeader";
 
 interface PurchaseAddedSectionProps {
-    purchaseRows: GridRowsProp,
-    purchaseSetRows: React.Dispatch<React.SetStateAction<GridRowsProp>>
+    // purchaseRows: GridRowsProp,
+    // purchaseSetRows: React.Dispatch<React.SetStateAction<GridRowsProp>>
 }
 
 const PurchaseAddedSection = (props: PurchaseAddedSectionProps) => {
-    const { purchaseRows, purchaseSetRows } = props;
+    // const { purchaseRows, purchaseSetRows } = props;
 
-    return (
-        <section>
-            <SectionHeader title="Purchase Added:" />
-            <PurchaseDataTable 
-                purchaseRows={purchaseRows}
-                purchaseSetRows={purchaseSetRows} 
-                isEditable={true} />
-        </section>
-    )
+    // return (
+    //     <section>
+    //         <SectionHeader title="Purchase Added:" />
+    //         <PurchaseDataTable 
+    //             purchaseRows={purchaseRows}
+    //             purchaseSetRows={purchaseSetRows} 
+    //             isEditable={true} />
+    //     </section>
+    // )
 }
 
 export default PurchaseAddedSection;
--- pages/sections/SalesAddedSection.tsx
+++ pages/sections/SalesAddedSection.tsx
@@ -1,25 +1,25 @@
-import { GridRowsProp } from "@mui/x-data-grid";
+// import { GridRowsProp } from "@mui/x-data-grid";
 import SalesDataTable from "../../components/SalesDataTable";
 import SectionHeader from "../../components/SectionHeader";
 
 interface SalesAddedSectionProps {
-  salesRows: GridRowsProp;
-  salesSetRows: React.Dispatch<React.SetStateAction<GridRowsProp>>;
+//   salesRows: GridRowsProp;
+//   salesSetRows: React.Dispatch<React.SetStateAction<GridRowsProp>>;
 }
 
 const SalesAddedSection = (props: SalesAddedSectionProps) => {
-  const { salesRows, salesSetRows } = props;
+//   const { salesRows, salesSetRows } = props;
 
-  return (
-    <section>
-      <SectionHeader title="Sales Added:" />
-      <SalesDataTable 
-        salesRows={salesRows}
-        salesSetRows={salesSetRows}
-        isEditable={true}
-      />
-    </section>
-  );
+//   return (
+//     <section>
+//       <SectionHeader title="Sales Added:" />
+//       <SalesDataTable 
+//         salesRows={salesRows}
+//         salesSetRows={salesSetRows}
+//         isEditable={true}
+//       />
+//     </section>
+//   );
 };
 
 export default SalesAddedSection;
